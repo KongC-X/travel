00f7a0573b5aa167093fd5433eb52e073c42c6b1
zhangyiming <931993416@qq.com>
2020-06-26 13:08:22 +0800

    index finish

--- Travel/src/pages/home/components/Icons.vue
+++ Travel/src/pages/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper>
+    <swiper  :options="swiperOptions">
       <swiper-slide v-for="(page, index) of pages" :key="index">
       <div class="icon" v-for="item of page" :key="item.id">
       <div class="icon-img">
@@ -16,59 +16,20 @@
 <script>
 export default {
   name: 'HomeIcons',
+  props: {
+    list: Array
+  },
   data () {
     return {
-      iconList: [{
-        id: '0001',
-        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/hotel.png',
-        desc: '酒店'
-      },
-      {
-        id: '0002',
-        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/flight.png',
-        desc: '飞机'
-      },
-      {
-        id: '0003',
-        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/train.png',
-        desc: '火车票'
-      },
-      {
-        id: '0004',
-        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/package.png',
-        desc: '度假'
-      },
-      {
-        id: '0005',
-        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/piao.png',
-        desc: '景点门票'
-      },
-      {
-        id: '0006',
-        imgUrl: 'https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png',
-        desc: '海外酒店'
-      },
-      {
-        id: '0007',
-        imgUrl: 'https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png',
-        desc: '自由行'
-      },
-      {
-        id: '0008',
-        imgUrl: 'https://picbed.qunarzz.com/1316dc82d1ce6259686d5a68880e5a9d.png',
-        desc: '攻略'
-      },
-      {
-        id: '0009',
-        imgUrl: 'https://picbed.qunarzz.com/377db8cb2143aebf01869c9baad3d325.png',
-        desc: '汽车票船票'
-      }]
+      swiperOptions: {
+        autoplay: false
+      }
     }
   },
   computed: {
     pages () {
       const pages = []
-      this.iconList.forEach((item, index) => {
+      this.list.forEach((item, index) => {
         const page = Math.floor(index / 8)
         if (!pages[page]) {
           pages[page] = []
--- Travel/src/pages/home/components/Swiper.vue
+++ Travel/src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-  <swiper :options="swiperOptions">
-    <swiper-slide v-for="item of swiperList" :key="item.id">
+  <swiper :options="swiperOptions" v-if="showSwiper">
+    <swiper-slide v-for="item of list" :key="item.id">
       <img class="swiper-img" :src="item.imgUrl">
     </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
@@ -12,26 +12,21 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOptions: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [
-        {
-          id: '0001',
-          imgUrl: '//imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
-        },
-        {
-          id: '0002',
-          imgUrl: '//imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
-        },
-        {
-          id: '0003',
-          imgUrl: '//imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
-        }
-      ]
+        // autoplay: true
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
