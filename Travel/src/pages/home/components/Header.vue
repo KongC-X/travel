5dd1602e3cd174beb57d79f767d08fdcace92ebd
zhangyiming <931993416@qq.com>
2020-06-26 13:56:48 +0800

    city-router

--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -6,9 +6,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题</div>
-    <div class="header-right">{{this.city}}
+    <router-link to="/city">
+    <div class="header-right">
+      {{this.city}}
       <span class="iconfont arrow-icon">&#xe600;</span>
-    </div>
+    </div></router-link>
+
   </div>
 </template>
 
@@ -25,7 +28,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display flex
-    line-height .86rem
+    line-height $headerHeight
     background-color $bgColor
     color #fff
     .header-left
@@ -48,6 +51,7 @@ export default {
       width 1.24rem
       float right
       text-align center
+      color #fff
       .arrow-icon
         margin-left -.04rem
         font-size .24rem
